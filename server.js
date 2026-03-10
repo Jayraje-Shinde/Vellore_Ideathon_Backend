e06d7c9403b20e8b4edca80baa72bb59f9f5062b
Jayraje Shinde <zeyraje@gmail.com>
2026-03-10 12:54:41 +0530

    changes server.js for crashes in railways

--- server.js
+++ server.js
@@ -1,3 +1,10 @@
+process.on('unhandledRejection', (err) => {
+  console.error('Unhandled Rejection:', err)
+})
+
+process.on('uncaughtException', (err) => {
+  console.error('Uncaught Exception:', err)
+})
 const express = require("express");
 const http = require("http");
 const { Server } = require("socket.io");
